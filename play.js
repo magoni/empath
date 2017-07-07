57703caf3c621240fec196d72cb79ce558830ea0
Ian Gibbons <ianjgibbons@gmail.com>
2017-07-06 22:23:49 -0400

    ch2 volume

--- play.js
+++ play.js
@@ -17,13 +17,20 @@ ch[1].pingPong = new Tone.PingPongDelay(0.3, 0.4);
 ch[1].vol = new Tone.Volume(0);
 ch[1].grainSrc.chain(ch[1].pingPong, ch[1].vol, Tone.Master);
 
-ch[2].samplerSrc = new Tone.Sampler("./audio/evan/2_12.wav").toMaster();
+ch[2].samplerSrc = new Tone.Sampler("./audio/evan/2_12.wav");
 ch[2].convolver = new Tone.Convolver("./audio/desert.wav");
+ch[2].vol = new Tone.Volume(0);
+ch[2].samplerSrc.chain(ch[2].convolver, ch[2].vol, Tone.Master);
 
 ch[1].vol.mute = true;
 ch[2].convolver.wet = 100;
 
 var loop = new Tone.Loop(function(time){
+	ch[1].grainSrc.scrub(Tone.Time(Math.random(10)*3)+1);
+	ch[2].samplerSrc.triggerAttack(-5);
+}, 0.4 ).start(0);
+
+var loop2 = new Tone.Loop(function(time){
 	ch[1].grainSrc.scrub(Tone.Time(Math.random(10)*3)+1);
 	ch[2].samplerSrc.triggerAttack(-5);
 }, 0.4 ).start(0);
